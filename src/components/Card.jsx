7b98864751a1a562c8c21053685ee4052bc53829
Tony Li <tonyli7119@gmail.com>
2023-12-21 03:35:36 -0500

    update on experience page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,60 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
 function Card({ data }) {
+  const [display, setDisplay] = useState(false);
+  const [showDisplay, setShowDisplay] = useState(showLogo());
+  const details = data.detail.split(", ");
+
+  function showCard() {
+    setDisplay(!display);
+  }
+
+  useEffect(() => {
+    if (display == false) {
+      setShowDisplay(showLogo());
+    }
+    if (display == true) {
+      setShowDisplay(showData());
+    }
+  }, [display]);
+
+  function showData() {
+    return (
+      <>
+        <div
+          onClick={showCard}
+          key={data.id}
+          className="bg-black text-white flex flex-col items-center justify-center w-64 h-96 cursor-pointer hover:scale-110 duration-200"
+        >
+          <div className="text-xl font-bold ">{data.companyTitle}</div>
+          <p className="pb-5">({data.time})</p>
+          {details.map((details) => (
+            <>
+              <li className="px-10">{details}</li>
+            </>
+          ))}
+        </div>
+      </>
+    );
+  }
+
+  function showLogo() {
+    return (
+      <>
+        <div
+          key={data.id}
+          onClick={showCard}
+          className="bg-white flex items-center justify-center w-64 h-96 cursor-pointer hover:scale-110 duration-200"
+        >
+          <img key={data.id} src={data.logo}></img>
+        </div>
+      </>
+    );
+  }
+
   return (
     <>
-      <div
-        key={data.id}
-        src={data.logo}
-        className="bg-white flex items-center justify-center w-48 h-60 cursor-pointer hover:scale-110 duration-200"
-      >
-        {data.companyTitle}
-      </div>
+      <div>{showDisplay}</div>
     </>
   );
 }
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -6,38 +6,47 @@ function Experience() {
     {
       id: "starbucks",
       companyTitle: "Starbucks",
-      logo: "",
-      cardClicked: false,
+      logo: "https://1000logos.net/wp-content/uploads/2023/04/Starbucks-logo-500x281.png",
+      detail: "details",
+      time: "July 2023 - Present",
     },
     {
       id: "yayatea",
       companyTitle: "Yaya Tea",
-      logo: "",
-      cardClicked: false,
+      logo: "https://yayatea.com/wp-content/uploads/2022/02/cropped-yaya-hi.png",
+      detail:
+        "Managed monetary transaction using cash register, Prepared beverages for customers quickly and effectively, Maintain a clean area for customers/coworkers, Greeting customers and providing customer service to customers",
+      time: "April 2022 - July 2023",
     },
     {
       id: "hunan",
-      companyTitle: "Hunan Chinese Resturant",
-      logo: "",
-      cardClicked: false,
+      companyTitle: "Hunan Chinese Restaurant",
+      logo: "https://t3.ftcdn.net/jpg/06/15/38/38/240_F_615383830_0C5bOBwUz9pMx9iZGT8VAuvbLX1MWisk.jpg",
+      detail:
+        "Managed monetary transaction using cash register, Packaging products for pickups/deliveries, Restocking establishment with necessary products",
+      time: "July 2021 - September 2021",
     },
     {
       id: "adventurerpark",
       companyTitle: "Adventurer's Park",
-      logo: "",
-      cardClicked: false,
+      logo: "https://adventurerspark.com/wp-content/uploads/2020/03/logo.png",
+      detail:
+        "Managed monetary transaction using cash, Restocked miscellaneous items such as prizes, Operated work stations in safe environments, Provided high quality customer service to customers",
+      time: "June 2019 - November 2019",
     },
     {
       id: "funfunsaturday",
       companyTitle: "Fun Fun Saturday",
-      logo: "",
-      cardClicked: false,
+      logo: "https://www.ueaa.org/wp-content/uploads/2022/01/UeaaBlue_logo-title-1.svg",
+      detail:
+        "Assisted in two classes with students ranging from 6 - 13 (Kickball and English), Ensured a safe environment during events like Sport Day, Prepared physical and literary elements for classroom experience",
+      time: "June 2018 - August 2018",
     },
   ];
 
   return (
     <>
-      <div className="px-10 h-screen bg-gray-400 flex items-center justify-between">
+      <div className="px-10 w-screen h-screen bg-gray-400 flex items-center justify-between">
         {experienceData.map((experienceData) => (
           <div key={experienceData.id}>
             <Card data={experienceData}></Card>
